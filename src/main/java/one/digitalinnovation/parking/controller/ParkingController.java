e92286fcb54c3a3593f1fc215f87429cf373cf48
Pedro <pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.com>
2022-11-13 10:55:06 -0300

    primeiro endpoint de cadastro

---
+++ src/main/java/one/digitalinnovation/parking/controller/ParkingController.java
@@ -0,0 +1,27 @@
+package one.digitalinnovation.parking.controller;
+
+import one.digitalinnovation.parking.model.Parking;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/parking")
+public class ParkingController {
+
+    @GetMapping
+    public List<Parking> findAll() {
+
+        var parking = new Parking();
+        parking.setColor("PRETO");
+        parking.setLicense("MSS-1111");
+        parking.setModel("VW Golf");
+        parking.setState("SP");
+
+        return Arrays.asList(parking);
+
+    }
+}
