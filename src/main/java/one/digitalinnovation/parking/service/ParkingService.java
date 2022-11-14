11230fa6ba610b15e873e58d30ccf874841cc8a2
Pedro <pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.com>
2022-11-14 11:22:11 -0300

    Formatando a EntryDate e finalizando o mapper

--- src/main/java/one/digitalinnovation/parking/service/ParkingService.java
+++ src/main/java/one/digitalinnovation/parking/service/ParkingService.java
@@ -3,6 +3,7 @@ package one.digitalinnovation.parking.service;
 import one.digitalinnovation.parking.model.Parking;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -13,8 +14,14 @@ public class ParkingService {
 
     static {
         var id = getUUID(); //Gera uma id aleatoria para o parking
+        var id2 = getUUID(); //Gera uma id aleatoria para o parking
+        var id3 = getUUID(); //Gera uma id aleatoria para o parking
         Parking parking = new Parking(id, "DMS-1111", "SC", "CELTA", "PRETO");
+        Parking parking2 = new Parking(id2, "ABC-1234", "SP", "GOLF", "VERMELHO");
+        Parking parking3 = new Parking(id3, "SDE-4356", "PE", "SAVEIRO", "BRANCO");
         parkingMap.put(id, parking);
+        parkingMap.put(id2, parking2);
+        parkingMap.put(id3, parking3);
     }
 
     public List<Parking> findAll(){
@@ -23,5 +30,18 @@ public class ParkingService {
 
     private static String getUUID() {
         return UUID.randomUUID().toString().replace("-","");
+    }
+
+    public Parking findById(String id) {
+        return parkingMap.get(id);
+    }
+
+    public Parking create(Parking parkingCreate) {
+        String uuid = getUUID();
+        parkingCreate.setId(uuid);
+        parkingCreate.setEntryDate(LocalDateTime.now());
+        parkingMap.put(uuid, parkingCreate);
+
+        return parkingCreate;
     }
 }
